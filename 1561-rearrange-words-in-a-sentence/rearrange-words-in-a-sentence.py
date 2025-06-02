834bcdc1c8536ae01e97740e96acb0762e0f8b23
Vijaykumar830 <161339899+Vijaykumar830@users.noreply.github.com>
2025-06-02 10:31:21 +0530

    Time: 3 ms (97.20%) | Memory: 19.3 MB (92.52%) - LeetSync

---
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrangeWords(self, text: str) -> str:
+        words = text.split(" ")
+        words[0] = words[0].lower()
+        words.sort(key=len)
+        words[0] = words[0][0].upper() + words[0][1:]
+        return " ".join(words)
+            
